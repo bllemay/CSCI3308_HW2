0287dd1734128923fbaac1cd8771cd0b1be0ecee
nhenault <nahe4217@colorado.edu>
2020-02-27 15:27:53 -0700

    Finished adding cards, need to add remove funciton

--- manage-cards.js
+++ manage-cards.js
@@ -5,20 +5,31 @@
 */
 function addCard(cardID)
 {
-  // Find the card referenced
-  card = document.getElementByID(cardID);
+    // Find the card referenced
+    editCard = document.getElementById(cardID);
 
-  // Create a new tweet card
-  var tweet = document.createElement('div');
-  tweet.className = "card";
-  // newAnchor.addEventListener('click', function()
-  // {
-  //   switchPlayers(i);
-  // });
-  tweet.innerHTML = <img class="card-img-top" src="https://cdn2.iconfinder.com/data/icons/social-media-square-set/960/Twitter_Sq-512.png" alt="Twitter Logo">
-                    <div class="card-body">
-                      <h5 class="card-title text-right">Tweet</h5>
-                      <p class="card-text text-right">Sample tweet will go here.</p>
-                    </div>;
-  card.appendChild(tweet);
+    // Create a new tweet card
+    var tweet = document.createElement("div");
+    tweet.className = "card";
+    tweet.innerHTML = `
+        <div class="text-right">
+            <button type="button" class="btn btn-danger" onClick="removeCard('interest2body')">Remove</button>
+        </div>
+        <img class="card-img-top" src="https://cdn2.iconfinder.com/data/icons/social-media-square-set/960/Twitter_Sq-512.png" alt="Twitter Logo">
+        <div class="card-body">
+            <h5 class="card-title text-right">Tweet</h5>
+            <p class="card-text text-right">Sample tweet will go here.</p>
+        </div>
+        `;
+    editCard.appendChild(tweet);
+}
+
+
+/*
+ @Return void
+ @Param cardID - The ID of the card being added to
+ The methods adds an additional card (to store a tweet) to the body of the card whose ID was passed in
+*/
+function removeCard(cardID)
+{
 }
